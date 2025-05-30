a7edb52b8773adc3345d3ab78ba83054ae731693
Saswat Das <107359627+Lostlyyy@users.noreply.github.com>
2025-05-30 18:16:36 +0530

    Update functions.sh

--- common/functions.sh
+++ common/functions.sh
@@ -148,7 +148,7 @@ mount_mirrors() {
 
 # Credits
 ui_print "**************************************"
-ui_print "*   MMT Extended by Zackptg5 @ XDA   *"
+ui_print "*  Camera Quality Fix for Realme X7 Max/GT Neo   *"
 ui_print "**************************************"
 ui_print " "
 
